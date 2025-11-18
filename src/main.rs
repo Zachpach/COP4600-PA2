c10e6604a38e1f7aef75698c5c921220c17f14b0
Zachary Frederick <zachpack200@gmail.com>
2025-11-18 13:54:14 -0500

    added a prints statements

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::LinkedList;
-use std::sync::Mutex;
 use std::string::String;
+use std::sync::Mutex;
 
-struct BucketNode<> {
+struct BucketNode {
     hash: usize,
     name: String,
     salary: u32,
@@ -21,7 +21,10 @@ impl BucketNode {
     }
 
     fn to_string(&self) -> String {
-       return format!("Node [ hash: {}, name: {}, salary: {} ] ", self.hash, self.name, self.salary);
+        return format!(
+            "Node [ hash: {}, name: {}, salary: {} ] ",
+            self.hash, self.name, self.salary
+        );
     }
 }
 
@@ -30,14 +33,14 @@ struct Bucket {
 }
 impl Bucket {
     fn new() -> Self {
-        return Bucket{
+        return Bucket {
             bucket: Mutex::new(LinkedList::new()),
         };
     }
     fn get(&self, p0: usize) -> &mut Bucket {
         todo!()
     }
-    fn to_string(&self) -> String{
+    fn to_string(&self) -> String {
         let bucket = self.bucket.lock().unwrap();
         let mut s1 = String::from("Bucket: { ");
         for element in bucket.iter() {
@@ -55,18 +58,22 @@ struct ConcurrentHashTable {
 
 impl ConcurrentHashTable {
     fn new(size: usize) -> ConcurrentHashTable {
+        println!("buckets: {}", size);
+        let mut temp:Vec<Bucket> = Vec::with_capacity(size);
+        println!("buckets: {}", temp.len());
+
+        for i in 0..size {
+            let bucket = Bucket { bucket: Mutex::new(LinkedList::new()), };
+            temp.push(bucket)
+        }
+
         return ConcurrentHashTable {
-            buckets: Vec::with_capacity(size),
+            buckets: temp,
         };
     }
-    fn insert(&mut self, bucket: Bucket) {
-
-    }
-    fn delete(&mut self, bucket: &Bucket) {
-
-    }
+    fn insert(&mut self, bucket: Bucket) {}
+    fn delete(&mut self, bucket: &Bucket) {}
     fn search_by_hash(&self, hash: usize) -> &mut Bucket {
-
         return self.buckets[hash].get(hash);
     }
     fn search_by_name(&self, name: String) -> &mut Bucket {
@@ -75,8 +82,10 @@ impl ConcurrentHashTable {
     }
 
     fn to_string(&self) -> String {
-        let mut s1 = String::from("Bucket: { \n");
+        let mut s1 = String::from("HashTable: { \n");
+        println!("buckets: {}", self.buckets.len());
         for element in self.buckets.iter() {
+            println!("1");
             s1.push_str(element.to_string().as_str());
             s1.push_str(", \n");
         }
@@ -87,15 +96,14 @@ impl ConcurrentHashTable {
 }
 
 fn main() {
-
-
-
+    let table = ConcurrentHashTable::new(10);
+    println!("{:?}", table.to_string());
 }
 
 fn jenkins_one_at_a_time_hash(key: String) -> usize {
     let mut i: usize = 0;
     let mut hash: usize = 0;
-    while (i != key.len()) {
+    while i != key.len() {
         hash += key.as_bytes()[i] as usize;
         hash += hash << 10;
         hash ^= hash >> 6;
